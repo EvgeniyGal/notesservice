ef3e39e378b5cfccf593ed4391ec53bf5a7edeb2
RomanVoznyy <roman.voznyy.52@gmail.com>
2023-07-14 18:37:19 +0300

    fixed controller and styles

--- src/main/java/edu/goit/notesservice/note/NoteController.java
+++ src/main/java/edu/goit/notesservice/note/NoteController.java
@@ -1,11 +1,13 @@
 package edu.goit.notesservice.note;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/note")
+@Controller
 @RequiredArgsConstructor
 public class NoteController {
     private final NoteService noteService;
