d0634121abcb8ec211826cfca5fe3ad1d62f4c66
RomanVoznyy <roman.voznyy.52@gmail.com>
2023-07-16 21:12:11 +0300

    added adding and edidting service

--- src/main/java/edu/goit/notesservice/note/NoteRepository.java
+++ src/main/java/edu/goit/notesservice/note/NoteRepository.java
@@ -1,8 +1,14 @@
 package edu.goit.notesservice.note;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, String> {
+
+    @Query(value = "SELECT * FROM NOTES WHERE user_id=?", nativeQuery = true)
+    public List<Note> findByUserId(int user_id);
 }
--- src/main/java/edu/goit/notesservice/note/NoteService.java
+++ src/main/java/edu/goit/notesservice/note/NoteService.java
@@ -1,16 +1,54 @@
 package edu.goit.notesservice.note;
 
+import edu.goit.notesservice.auth.User;
+import edu.goit.notesservice.auth.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class NoteService {
     private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
 
     public List<Note> listAll() {
-        return noteRepository.findAll();
+        //temporary search for User with id 1
+        return noteRepository.findByUserId(1);
+    }
+
+    public Note add(Note note) {
+        note.setId(UUID.randomUUID().toString());
+        //temporary all notes to User 1
+        User user = userRepository.getReferenceById(1);
+        note.setUser(user);
+        return noteRepository.save(note);
+    }
+
+    public boolean isExist(String id) {
+        return noteRepository.existsById(id);
+    }
+
+    public void deleteById(String id) {
+        if(!isExist(id)){
+            throw new IllegalArgumentException("Нотатка з id '" + id + "' не існує");
+        }
+        noteRepository.deleteById(id);
+    }
+
+    public void update(Note note) {
+        //temporary all notes to User 1
+        User user = userRepository.getReferenceById(1);
+        note.setUser(user);
+        noteRepository.save(note);
+    }
+
+    public Note getById(String id) {
+        if(!isExist(id)){
+            throw new IllegalArgumentException("Нотатка з id '" + id + "' не існує");
+        }
+        return noteRepository.getReferenceById(id);
     }
 }
